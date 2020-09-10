0394d9087821c9ab951ea287b293b28cda5b996b
Jacob Farkas <jacobf@rkas.net>
2020-09-10 01:49:42 -0700

    Get the Ubuntu version of the playbook working

--- ubuntu/test.sh
+++ ubuntu/test.sh
@@ -3,9 +3,9 @@
 containerID=`docker run --rm -d -p 2222:22 \
             -v "$HOME/.ssh/id_rsa.pub":/root/.ssh/authorized_keys:ro \
             -v "$HOME/.ssh/id_rsa.pub":/etc/authorized_keys/alpine:ro \
-           farktronix/ansible_dummy:latest-alpine`
-ansible-playbook ./playbook.yml -i 127.0.0.1, -u alpine --ssh-extra-args "-p 2222" -K
+           farktronix/ansible_dummy:latest-ubuntu`
+ansible-playbook ./playbook.yml -i 127.0.0.1, -u ubuntu --ssh-extra-args "-p 2222" -K
 echo "Alpine image is set up! Log in and test it with this command:"
-echo "ssh -p 2222 alpine@localhost"
+echo "ssh -p 2222 ubuntu@localhost"
 read -n 1 -p "Press any key to continue. The docker container will be destroyed." garbage
 docker stop $containerID
