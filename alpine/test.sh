7988c386a90144aaa3ac91902b33fd5bc2bd9271
Jacob Farkas <jacobf@rkas.net>
2020-09-10 00:25:29 -0700

    Added a test script Set fish as the default shell Start work on an Ubuntu version

---
+++ alpine/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+containerID=`docker run --rm -d -p 2222:22 \
+            -v "$HOME/.ssh/id_rsa.pub":/root/.ssh/authorized_keys:ro \
+            -v "$HOME/.ssh/id_rsa.pub":/etc/authorized_keys/alpine:ro \
+           farktronix/alpine_ansible:latest`
+ansible-playbook ./playbook.yml -i 127.0.0.1, -u alpine --ssh-extra-args "-p 2222" -K
+echo "Alpine image is set up! Log in and test it with this command:"
+echo "ssh -p 2222 alpine@localhost"
+read -n 1 -p "Press any key to continue. The docker container will be destroyed." garbage
+docker stop $containerID
